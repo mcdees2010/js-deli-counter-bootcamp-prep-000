4ccb244436bf4738b00b280d8f459bd246ab0350
Maria Dees <kingsgrl2010@yahoo.com>
2018-04-15 22:55:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,4 +17,11 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
- 
+ for( i = 0; i < katzDeliLine.length; i++)
+  if( i > 0 ) {
+    return(`The line is currently: ${katzDeliLine}"`)
+    else{
+      print("The line is currently empty.")
+    }
+  }
+}
